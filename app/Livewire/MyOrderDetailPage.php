f0573266fa6342e1d7fbceb3d65d5c9306fc7ff8
BU-Aaron <aaronjoncardel.alberto@bicol-u.edu.ph>
2025-01-06 13:06:46 +0800

    Implement order detail page with dynamic data retrieval and improved layout
    - Added functionality to MyOrderDetailPage to retrieve order details, items, and address based on the order ID.
    - Enhanced the OrderItem model to establish a relationship with the Product model for better data access.
    - Updated the my-order-detail-page view to display order information, including user name, order date, status, items, and shipping address dynamically.
    - Improved the summary section to reflect the total price in PHP currency format.
    
    These changes enhance the user experience by providing detailed order information and a more interactive interface.

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Address;
+use App\Models\Shop\Order;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Order Detail')]
 class MyOrderDetailPage extends Component
 {
+    public $order_id;
+
+    public function mount($order_id)
+    {
+        $this->order_id = $order_id;
+    }
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        $order = Order::with('items.product')->find($this->order_id);
+        $order_items = $order ? $order->items : [];
+        $address = $order ? $order->address : [];
+
+        return view('livewire.my-order-detail-page', [
+            'order' => $order,
+            'order_items' => $order_items,
+            'address' => $address,
+        ]);
     }
 }
--- resources/views/livewire/my-order-detail-page.blade.php
+++ resources/views/livewire/my-order-detail-page.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
     <h1 class="text-4xl font-bold text-slate-500">Order Details</h1>
-  
+
     <!-- Grid -->
     <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 mt-5">
       <!-- Card -->
@@ -14,7 +14,7 @@
               <path d="M16 3.13a4 4 0 0 1 0 7.75" />
             </svg>
           </div>
-  
+
           <div class="grow">
             <div class="flex items-center gap-x-2">
               <p class="text-xs uppercase tracking-wide text-gray-500">
@@ -22,13 +22,13 @@
               </p>
             </div>
             <div class="mt-1 flex items-center gap-x-2">
-              <div>Jace Grimes</div>
+              <div>{{ $order->user->name }}</div>
             </div>
           </div>
         </div>
       </div>
       <!-- End Card -->
-  
+
       <!-- Card -->
       <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
         <div class="p-4 md:p-5 flex gap-x-4">
@@ -40,7 +40,7 @@
               <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
             </svg>
           </div>
-  
+
           <div class="grow">
             <div class="flex items-center gap-x-2">
               <p class="text-xs uppercase tracking-wide text-gray-500">
@@ -49,14 +49,14 @@
             </div>
             <div class="mt-1 flex items-center gap-x-2">
               <h3 class="text-xl font-medium text-gray-800 dark:text-gray-200">
-                17-02-2024
+                {{ $order->created_at->format('d-m-Y') }}
               </h3>
             </div>
           </div>
         </div>
       </div>
       <!-- End Card -->
-  
+
       <!-- Card -->
       <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
         <div class="p-4 md:p-5 flex gap-x-4">
@@ -66,7 +66,7 @@
               <path d="m12 12 4 10 1.7-4.3L22 16Z" />
             </svg>
           </div>
-  
+
           <div class="grow">
             <div class="flex items-center gap-x-2">
               <p class="text-xs uppercase tracking-wide text-gray-500">
@@ -74,13 +74,13 @@
               </p>
             </div>
             <div class="mt-1 flex items-center gap-x-2">
-              <span class="bg-yellow-500 py-1 px-3 rounded text-white shadow">Processing</span>
+              <span class="bg-yellow-500 py-1 px-3 rounded text-white shadow">{{ $order->status->value }}</span>
             </div>
           </div>
         </div>
       </div>
       <!-- End Card -->
-  
+
       <!-- Card -->
       <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
         <div class="p-4 md:p-5 flex gap-x-4">
@@ -92,7 +92,7 @@
               <path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2" />
             </svg>
           </div>
-  
+
           <div class="grow">
             <div class="flex items-center gap-x-2">
               <p class="text-xs uppercase tracking-wide text-gray-500">
@@ -108,7 +108,7 @@
       <!-- End Card -->
     </div>
     <!-- End Grid -->
-  
+
     <div class="flex flex-col md:flex-row gap-4 mt-4">
       <div class="md:w-3/4">
         <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
@@ -122,75 +122,64 @@
               </tr>
             </thead>
             <tbody>
-  
-              <!--[if BLOCK]><![endif]-->
-              <tr wire:key="53">
-                <td class="py-4">
-                  <div class="flex items-center">
-                    <img class="h-16 w-16 mr-4" src="http://localhost:8000/storage/products/01HND3J5XS7ZC5J84BK5YDM6Z2.jpg" alt="Product image">
-                    <span class="font-semibold">Samsung Galaxy Watch6</span>
-                  </div>
-                </td>
-                <td class="py-4">₹29,999.00</td>
-                <td class="py-4">
-                  <span class="text-center w-8">1</span>
-                </td>
-                <td class="py-4">₹29,999.00</td>
-              </tr>
-              <tr wire:key="54">
-                <td class="py-4">
-                  <div class="flex items-center">
-                    <img class="h-16 w-16 mr-4" src="http://localhost:8000/storage/products/01HND30J0P7C6MWQ1XQK7YDQKA.jpg" alt="Product image">
-                    <span class="font-semibold">Samsung Galaxy Book3</span>
-                  </div>
-                </td>
-                <td class="py-4">₹75,000.00</td>
-                <td class="py-4">
-                  <span class="text-center w-8">5</span>
-                </td>
-                <td class="py-4">₹375,000.00</td>
-              </tr>
-              <!--[if ENDBLOCK]><![endif]-->
-  
+                @foreach ($order_items as $item)
+                <tr wire:key="{{ $item->id }}">
+                    <td class="py-4">
+                        <div class="flex items-center">
+                            <img class="h-16 w-16 mr-4" src="{{ $item->product->getFirstMediaUrl('product-images') }}" alt="Product image">
+                            <span class="font-semibold">{{ $item->product->name }}</span>
+                        </div>
+                    </td>
+                    <td class="py-4">PHP {{ number_format($item->unit_price, 2) }}</td>
+                    <td class="py-4">
+                        <span class="text-center w-8">{{ $item->qty }}</span>
+                    </td>
+                    <td class="py-4">PHP {{ number_format($item->unit_price * $item->qty, 2) }}</td>
+                </tr>
+                @endforeach
             </tbody>
           </table>
         </div>
-  
+
         <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
           <h1 class="font-3xl font-bold text-slate-500 mb-3">Shipping Address</h1>
-          <div class="flex justify-between items-center">
-            <div>
-              <p>42227 Zoila Glens, Oshkosh, Michigan, 55928</p>
-            </div>
-            <div>
-              <p class="font-semibold">Phone:</p>
-              <p>023-509-0009</p>
-            </div>
-          </div>
+          @if($address)
+              <div class="flex justify-between items-center">
+                  <div>
+                      <p>{{ $address->street_address }}, {{ $address->city }}, {{ $address->state }}, {{ $address->zip_code }}</p>
+                  </div>
+                  <div>
+                      <p class="font-semibold">Phone:</p>
+                      <p>{{ $address->phone }}</p>
+                  </div>
+              </div>
+          @else
+              <p>No shipping address available.</p>
+          @endif
         </div>
-  
+
       </div>
       <div class="md:w-1/4">
         <div class="bg-white rounded-lg shadow-md p-6">
           <h2 class="text-lg font-semibold mb-4">Summary</h2>
           <div class="flex justify-between mb-2">
             <span>Subtotal</span>
-            <span>₹404,999.00</span>
+            <span>PHP {{ number_format($order->total_price, 2) }}</span>
           </div>
           <div class="flex justify-between mb-2">
             <span>Taxes</span>
-            <span>₹0.00</span>
+            <span>PHP 0.00</span>
           </div>
           <div class="flex justify-between mb-2">
             <span>Shipping</span>
-            <span>₹0.00</span>
+            <span>PHP 0.00</span>
           </div>
           <hr class="my-2">
           <div class="flex justify-between mb-2">
             <span class="font-semibold">Grand Total</span>
-            <span class="font-semibold">₹404,999.00</span>
+            <span class="font-semibold">PHP {{ number_format($order->total_price, 2) }}</span>
           </div>
-  
+
         </div>
       </div>
     </div>
