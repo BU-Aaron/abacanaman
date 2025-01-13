8c58b86cd876484a006c9b2e5c1fe48671be119d
BU-Aaron <aaronjoncardel.alberto@bicol-u.edu.ph>
2025-01-14 02:09:35 +0800

    Refactor Products Page and Footer for Improved Clarity
    - Removed the Product Status Filter section from the products page to streamline the layout and focus on product listings.
    - Eliminated the 'Featured Products' link from the footer, reducing redundancy and enhancing navigation clarity.
    
    These changes simplify the user interface, making it more intuitive for users to navigate the products section.

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -33,20 +33,6 @@
                     @endforeach
                 </ul>
             </div>
-
-            <!-- Product Status Filter -->
-            <div class="p-4 mb-5 bg-white border border-gray-200 dark:bg-gray-900 dark:border-gray-900">
-                <h2 class="text-2xl font-bold dark:text-gray-400">Product Status</h2>
-                <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
-                <ul>
-                    <li class="mb-4">
-                        <label for="featured" class="flex items-center dark:text-gray-300">
-                            <input type="checkbox" id="featured" wire:model.live="featured" value="1" class="w-4 h-4 mr-2">
-                            <span class="text-lg dark:text-gray-400">Featured Products</span>
-                        </label>
-                    </li>
-                </ul>
-            </div>
         </div>
 
         <!-- Products Listing -->
