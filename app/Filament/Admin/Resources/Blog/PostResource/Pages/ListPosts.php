d904a93b285516abbe9058e61580d3ec8aa47176
BU-Aaron <aaronjoncardel.alberto@bicol-u.edu.ph>
2025-01-13 12:24:27 +0800

    Remove Create Action from ListPosts Page to Simplify User Interface
    - Eliminated the CreateAction from the ListPosts page, resulting in a cleaner and more focused user interface for blog post management.
    - This change enhances usability by reducing clutter and allowing users to concentrate on existing blog records.

--- app/Filament/Admin/Resources/Blog/PostResource/Pages/ListPosts.php
+++ app/Filament/Admin/Resources/Blog/PostResource/Pages/ListPosts.php
@@ -12,8 +12,6 @@ class ListPosts extends ListRecords
 
     protected function getActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        return [];
     }
 }
