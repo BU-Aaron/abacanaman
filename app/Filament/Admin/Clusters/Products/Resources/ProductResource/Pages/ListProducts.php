1629c2bd23694ef23d431158d2185b3c9908edbb
BU-Aaron <aaronjoncardel.alberto@bicol-u.edu.ph>
2025-01-13 12:20:48 +0800

    Remove Create Action from ListProducts Page to Simplify Interface
    - Eliminated the CreateAction from the ListProducts page, resulting in a cleaner and more focused user interface for product management.
    - This change enhances usability by reducing clutter and allowing users to concentrate on existing product records.

--- app/Filament/Admin/Clusters/Products/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Admin/Clusters/Products/Resources/ProductResource/Pages/ListProducts.php
@@ -15,9 +15,7 @@ class ListProducts extends ListRecords
 
     protected function getActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        return [];
     }
 
     protected function getHeaderWidgets(): array
